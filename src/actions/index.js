893a372233168ca01128f47f8d1365f399cae428
Jack Mocherman <mochermanjack@gmail.com>
2016-10-21 16:05:43 -0500

    built fetch weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios'
+
 const API_KEY = '8979670765e2d45cd0fc9052bf3f35ee'
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`
+	const request = axios.get(url)
+
+	return {
+		type: FETCH_WEATHER
+		payload: request
+	}
+}

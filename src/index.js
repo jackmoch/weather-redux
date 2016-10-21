73fdb2050c3dccaf5cf5a4cea684adf8bbe2bd3b
Jack Mocherman <mochermanjack@gmail.com>
2016-10-21 16:04:44 -0500

    added redux promise to middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import ReduxPromise from 'redux-promise'
 
 import App from './components/app'
 import reducers from './reducers'
 
-const createStoreWithMiddleWare = applyMiddleware()(createStore)
+const createStoreWithMiddleWare = applyMiddleware(ReduxPromise)(createStore)
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleWare(reducers)}>
